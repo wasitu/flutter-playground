f12b913ef2c018fc9864391db894b02898a2ca89
wasitu <wasitu@users.noreply.github.com>
2021-03-14 09:35:07 +0000

    deploy: 1b0d4c377cdb924276fe11964fb4ac33a80428f8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "3404dbf8842dc52d61ae43a116ebfe34",
+  "main.dart.js": "a0b00fe8111b2f38935a2682609ca479",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"assets/NOTICES": "482e555fb28ed482ea100c87d0a02f56",
+"assets/NOTICES": "45335e423b14924a87b43c3ac37b7509",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
@@ -15,8 +15,8 @@ const RESOURCES = {
 "version.json": "a03d6749df8f2634cd0b77d669d121db",
 "manifest.json": "b0455a8e72de63d4d4921d2842e436c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "66d0e0710d4a41a491b2a55e821e0c83",
-"/": "66d0e0710d4a41a491b2a55e821e0c83"
+"index.html": "5afbd1d513a2beb6f439fae40122a60a",
+"/": "5afbd1d513a2beb6f439fae40122a60a"
 };
 
 // The application shell files that are downloaded before a service worker can
