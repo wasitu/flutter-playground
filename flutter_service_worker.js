33a0415891ef952ff8d30f7c76fda423cafcc2ad
wasitu <wasitu@users.noreply.github.com>
2021-10-12 18:39:44 +0000

    deploy: 9130af32b31ebffc58d83835ca33b05d429df433

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,21 +3,21 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"firebase_config.js": "0410077490287eefa06ce74621ccb02c",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+  "firebase_config.js": "0410077490287eefa06ce74621ccb02c",
 "manifest.json": "b0455a8e72de63d4d4921d2842e436c1",
-"version.json": "a03d6749df8f2634cd0b77d669d121db",
-"assets/AssetManifest.json": "1142beb922fe3c70e6718d8c4c605bd9",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/FontManifest.json": "9e0649c90f16a7be3d51ab27d6089604",
+"version.json": "30dcd78847cf164c9e627664c63715e0",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"main.dart.js": "a448c8a87b6cf51242ee24bcd5f8ffb0",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_neumorphic/fonts/NeumorphicIcons.ttf": "32be0c4c86773ba5c9f7791e69964585",
-"assets/NOTICES": "6e3043c55434ad10d7e91c0f21bed450",
-"main.dart.js": "78d852267bdf81d232d17a2e12b7204b",
-"index.html": "c838dbd7a7a91761037e02a5693b9798",
-"/": "c838dbd7a7a91761037e02a5693b9798"
+"assets/FontManifest.json": "39018951616f8eadde6efcab968cc5f9",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "268f51e610d9dfef98d16658c3846427",
+"assets/AssetManifest.json": "1142beb922fe3c70e6718d8c4c605bd9",
+"index.html": "39ac32d2e634e35cdee6b290e799f79a",
+"/": "39ac32d2e634e35cdee6b290e799f79a"
 };
 
 // The application shell files that are downloaded before a service worker can
